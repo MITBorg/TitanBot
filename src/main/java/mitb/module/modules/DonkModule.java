1a614235d32672c4ed8bcfc9a31c68d41b014c68
dg36 <david.goldfinch@gmail.com>
2016-03-07 03:53:28 +0000

    fix for DonkModule

--- src/main/java/mitb/module/modules/DonkModule.java
+++ src/main/java/mitb/module/modules/DonkModule.java
@@ -6,6 +6,7 @@ import mitb.event.Listener;
 import mitb.event.events.CommandEvent;
 import mitb.event.events.MessageEvent;
 import mitb.module.Module;
+import mitb.util.Properties;
 
 /**
  * In loving memory of the previous bot that did this. Previous bot name not found.
@@ -15,7 +16,9 @@ public class DonkModule extends Module {
     @Listener
     public void onMessage(MessageEvent event) {
         org.pircbotx.hooks.events.MessageEvent evt = event.getOriginalEvent();
-        if(evt.getMessage().toLowerCase().contains("donk")) {
+        String msg = evt.getMessage().toLowerCase();
+        String botName = evt.getBot().getNick().toLowerCase();
+        if(msg.contains("donk") && !msg.startsWith(botName) && !evt.getUser().getNick().equals(botName)) {
             evt.respondWith("www.youtube.co.uk/watch?v=ckMvj1piK58");
         }
     }
